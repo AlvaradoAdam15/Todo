4ae6e98fcdd1c03b20d4d3dadd87726a03d9dd0d
adam Alvarado <adamalvarado@iesebre.com>
2015-10-25 16:18:38 +0100

    Add new task and show

--- VUE/app.js
+++ VUE/app.js
@@ -6,7 +6,32 @@ new Vue({
 
     data: {
         tasks:[
+
             { body: 'Go to the store', completed: false }
-        ]
+
+        ],
+
+        newTask:''
+    },
+
+    methods: {
+
+        addTask: function(e){
+
+            e.preventDefault();
+
+
+            this.tasks.push({
+
+                body: this.newTask,
+
+                completed: false
+
+            });
+
+            this.newTask = '';
+
+        }
     }
+
 })
