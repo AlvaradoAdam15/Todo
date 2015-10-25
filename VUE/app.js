288575676d90b75e6f9226df68bda6c5cb2b31d1
adam Alvarado <adamalvarado@iesebre.com>
2015-10-25 15:35:42 +0100

    Creating index.html and app.js

---
+++ VUE/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by adam on 25/10/15.
+ */
+new Vue({
+    el: '#task',
+
+    data: {
+        tasks:[
+            {body: 'Go to the store', completed: false}
+        ]
+    }
+})
